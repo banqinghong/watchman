5a8e4a099cb3bd65211d065475b55c02ea6d435f
xpand <banqinghong@126.com>
2018-07-10 13:48:46 +0800

    1.新增服务管理模块 2.主机信息新增别名显示

--- watchkeeper/admin.py
+++ watchkeeper/admin.py
@@ -1,6 +1,6 @@
 # _*_ coding:utf-8 _*_
 from django.contrib import admin
-from models import watchkeeper, serverInfo
+from models import watchkeeper, serverInfo, ServiceInfo
 
 admin.site.site_header = '盼达值班系统'
 admin.site.site_title = '盼达'
@@ -14,8 +14,13 @@ class opslist(admin.ModelAdmin):
 
 @admin.register(serverInfo)
 class serverList(admin.ModelAdmin):
-    list_display = ('ip', 'innerip', 'service', 'cpu', 'mem', 'role')
-
+    list_display = ('nickname', 'ip', 'innerip', 'get_service', 'cpu', 'mem', 'role')
+    #fields = ('ip', 'innerip', 'service')
+    filter_horizontal = ('service',)
     list_filter = ('role', 'cpu', 'mem')
-    search_fields = ('service', 'ip')
+    search_fields = ('ip', 'nickname', 'service__name')
 
+
+@admin.register(ServiceInfo)
+class ServiceList(admin.ModelAdmin):
+    list_display = ('name', 'packgeName')
--- watchkeeper/models.py
+++ watchkeeper/models.py
@@ -12,9 +12,9 @@ class watchkeeper(models.Model):
         (tag_need_not, '不需要')
     )
     name = models.CharField(default="", blank=True, verbose_name='姓名', max_length=30)
-    phone = models.CharField(verbose_name='电话',  blank=True, max_length=11, default="")
-    qq = models.CharField(verbose_name='QQ号',  blank=True, max_length=13, default="")
-    email = models.EmailField(verbose_name='邮箱',  blank=True, default="")
+    phone = models.CharField(verbose_name='电话', blank=True, max_length=11, default="")
+    qq = models.CharField(verbose_name='QQ号', blank=True, max_length=13, default="")
+    email = models.EmailField(verbose_name='邮箱', blank=True, default="")
     tag = models.IntegerField(default=tag_default, choices=tag_choice, verbose_name='是否需要值班')
 
     def __unicode__(self):
@@ -23,6 +23,18 @@ class watchkeeper(models.Model):
     class Meta:
         verbose_name = '运维人员'
         verbose_name_plural = '运维人员'
+
+
+class ServiceInfo(models.Model):
+    name = models.CharField(default="", blank=True, verbose_name='服务名称', max_length=30)
+    packgeName = models.CharField(default="", blank=True, verbose_name='包名称', max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '服务'
+        verbose_name_plural = '服务'
 
 
 class watchlist(models.Model):
@@ -51,17 +63,22 @@ class serverInfo(models.Model):
         (role_pd, '生产环境')
     )
     ip = models.GenericIPAddressField(verbose_name='IP地址')
-    innerip = models.GenericIPAddressField(default='127.0.0.1',  null=True,  blank=True, verbose_name='内网IP')
+    innerip = models.GenericIPAddressField(default='127.0.0.1', null=True, blank=True, verbose_name='内网IP')
     nickname = models.CharField(verbose_name='别名', max_length=20)
-    service = models.TextField(verbose_name='运行服务')
+    # service = models.TextField(verbose_name='运行服务')
+    service = models.ManyToManyField(ServiceInfo, blank=True, verbose_name=u'运行服务')
     cpu = models.IntegerField(verbose_name='CPU(核)')
     mem = models.IntegerField(verbose_name='内存（G）')
     system = models.TextField(verbose_name='操作系统')
     role = models.CharField(default=role_default, choices=role_choice, verbose_name='所属环境', max_length=20)
 
     class Meta:
-        verbose_name = '服务器'
-        verbose_name_plural = '服务器'
+        verbose_name = '主机'
+        verbose_name_plural = '主机'
+
+    def get_service(self):
+        return ",".join([p.name for p in self.service.all()])
+    get_service.short_description = '运行服务'
 
     def __unicode__(self):
         return self.ip
